04d81afc3e3c76131f756ecbaf13072fa76a7ec7
Dhruv251004 <118115413+Dhruv251004@users.noreply.github.com>
2024-10-03 21:25:31 +0530

    Partial footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Grid2 from '@mui/material/Grid2';
+import { ListItem } from '@mui/material';
+const Footer = () => {
+  return (
+    <Grid2 container direction={'column'}>
+        <Grid2>
+            Hi
+        </Grid2>
+        <Grid2>
+            <ListItem>Aayutan</ListItem>
+            <ListItem>Icons</ListItem>
+        </Grid2>
+    </Grid2>
+  )
+}
+
+export default Footer
