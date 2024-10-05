8b3d71656251f3bf621aa7aaafb5685d20bd9a1e
Aayushman-Gupta <aayushmangupta780@gmail.com>
2024-10-05 18:59:37 +0530

    improved the card structures

--- src/components/Cardsection.jsx
+++ src/components/Cardsection.jsx
@@ -8,37 +8,94 @@ import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 
 const card1 = (
-    <Card sx={{ minWidth: 500 }}>
-    <CardContent>
-      <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
-        Word of the Day
-      </Typography>
-      <Typography variant="h5" component="div">
-        benev
-      </Typography>
-      <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>adjective</Typography>
-      <Typography variant="body2">
-        well meaning and kindly.
-        <br />
-        {'"a benevolent smile"'}
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button size="small">Learn More</Button>
-    </CardActions>
-  </Card>
+  <Card sx={{ flexGrow: 1,
+    bgcolor:'#0928A4',
+    flexBasis: '30%', 
+    maxWidth: '25%',
+    color:'white',
+    borderRadius: '40px'}}>
+  <CardContent>
+    <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
+      Word of the Day
+    </Typography>
+    <Typography variant="h5" component="div">
+      benev
+    </Typography>
+    <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>adjective</Typography>
+    <Typography variant="body2">
+      well meaning and kindly.
+      <br />
+      {'"a benevolent smile"'}
+    </Typography>
+  </CardContent>
+  <CardActions>
+    <Button size="small">Learn More</Button>
+  </CardActions>
+</Card>
+);
+const card2 = (
+  <Card sx={{ flexGrow: 1,
+    bgcolor:'#0928A4',
+    flexBasis: '30%', 
+    maxWidth: '25%',
+    color:'white',
+    borderRadius: '40px'}}>
+  <CardContent>
+    <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
+      Word of the Day
+    </Typography>
+    <Typography variant="h5" component="div">
+      benev
+    </Typography>
+    <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>adjective</Typography>
+    <Typography variant="body2">
+      well meaning and kindly.
+      <br />
+      {'"a benevolent smile"'}
+    </Typography>
+  </CardContent>
+  <CardActions>
+    <Button size="small">Learn More</Button>
+  </CardActions>
+</Card>
+);
+const card3 = (
+  <Card sx={{ flexGrow: 1,
+    bgcolor:'#0928A4',
+    flexBasis: '30%', 
+    maxWidth: '25%',
+    color:'white',
+    borderRadius: '40px'}}>
+  <CardContent>
+    <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
+      Word of the Day
+    </Typography>
+    <Typography variant="h5" component="div">
+      benev
+    </Typography>
+    <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>adjective</Typography>
+    <Typography variant="body2">
+      well meaning and kindly.
+      <br />
+      {'"a benevolent smile"'}
+    </Typography>
+  </CardContent>
+  <CardActions>
+    <Button size="small">Learn More</Button>
+  </CardActions>
+</Card>
 );
 
 export default function Cards() {
   return (
     <Box className="mx-2 my-2" sx={{ borderColor: "red" }}>
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    {/* <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <Typography variant="h3">hellofn v evevevrevevfdm</Typography>
-    </div>
-    <Stack sx={{ justifyContent: "space-between", alignItems: "center" }} direction="row" spacing={2}>
+    </div> */}
+    <Stack sx={{ justifyContent: "space-between", alignItems: "center",marginTop:'100px' }} direction="row" spacing={2}>
       {card1}
-      {card1}
-      {card1}
+      {card2}
+      {card3}
     </Stack>
   </Box>
   
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,29 +1,30 @@
 import React from 'react'
 import Header from '../../components/Header'
-import TypographyContent from '../../components/typography'
 import CentreBody from '../../components/centreBody'
 import Footer from '../../components/Footer'
 import Cards from '../../components/Cardsection'
 import Card from '../../components/Card' 
 import cardData from '../../components/cardData.json'
+import VerticalCards from '../../components/verticalCards'
 function Home () {
   return (
-    <div>
-          <>
+    <>
         <div className='header'>
-    
-            <Header />
+             <Header />
         </div>
       <div className="centrebody">
-        <CentreBody/>
+             <CentreBody/>
       </div>
-      <Cards/>
-      {cardData.map((card, index) => (
+              {/* <Cards/> */}
+      <div>
+              <VerticalCards/>
+      </div>
+      
+      {/* {cardData.map((card, index) => (
                 <Card key={index} data={card} />
-            ))}
+            ))} */}
       <Footer />
     </>
-    </div>
   )
 }
 
