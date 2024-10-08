9856a951726e8e783be0b354b51305c99e92fa36
Aayushman-Gupta <aayushmangupta780@gmail.com>
2024-10-08 16:21:29 +0530

    working on the appointment system

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ function App() {
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/chat" element={<Chat/>}/>
-      <Route path="/appointment" element={<AppointmentFilterBar/>}/>
     </Routes>
     </BrowserRouter>
   )
